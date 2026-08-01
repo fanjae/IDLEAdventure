f4ca42eacaa79e820b3e826104da171499b33494
HooniRING <ssoh7079@gmail.com>
2026-08-01 18:17:33 +0900

    feat/unitdata
    UnitDataSO : 영웅과 적이 공통으로 사용하는 SO.
    HeroData : 영웅 데이터
    EnemyData : 적 데이터
    BattleTypes : enum타입들 모음(역할군, 상태, 공격방식)

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Game Data/Unit/Enemy Data")]
+public class EnemyData : UnitDataSO
+{
+    [Header("적 정보")]
+    [SerializeField] private bool isBoss;
+
+    public bool IsBoss => isBoss;
+}
---
+++ Assets/Scripts/Enums/BattleTypes.cs
@@ -0,0 +1,24 @@
+﻿public enum UnitTeam // 아군, 적군
+{
+    Hero,
+    Enemy
+}
+public enum UnitState // 상태
+{
+    Idle,
+    Move,
+    Attack,
+    Skill,
+    Dead
+}
+public enum HeroRole // 역할군
+{
+    Tanker,
+    Dealer,
+    Healer
+}
+public enum AttackType // 공격 방식(근거리, 원거리)
+{
+    Melee,
+    Ranged
+}
