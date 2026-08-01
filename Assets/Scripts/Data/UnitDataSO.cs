e176a18d7f309130a85e721dd71d9e9970c5ec28
HooniRING <ssoh7079@gmail.com>
2026-08-01 18:39:48 +0900

    chore : abstract 반영
    수정

--- Assets/Scripts/Data/UnitDataSO.cs
+++ Assets/Scripts/Data/UnitDataSO.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class UnitDataSO : ScriptableObject
+public abstract class UnitDataSO : ScriptableObject
 {
     [Header("기본 정보")]//영웅 데이터
     [SerializeField] private string unitID;//추후에 int로 교체할 수도 있음.(삭제할 가능성도 있음)
@@ -8,16 +8,16 @@ public class UnitDataSO : ScriptableObject
 
     [Header("전투 능력치")]
     [SerializeField, Min(1)] private int maxHp = 100;
-    [SerializeField, Min(1)] private int attack = 10; 
+    [SerializeField, Min(1)] private int attack = 10;
     [SerializeField, Min(0)] private int defense = 3;
 
     [Header("전투 설정")]
-    [SerializeField, Min(0.1f)] private float attackSpeed = 1.0f;
+    [SerializeField, Min(0.1f)] private float attackSpeed = 1.0f; //기본 공격속도
     [SerializeField, Min(0.1f)] private float attackRange = 1.5f;
     [SerializeField, Min(0.1f)] private float moveSpeed = 3.0f;
 
     [Header("공격 타입")]
-    //[SerializeField] private AttackType //공격 타입(근거리, 원거리)
+    [SerializeField] private AttackType attackType = AttackType.Melee;
 
     public string UnitID => unitID;
     public string UnitName => unitName;
@@ -30,5 +30,5 @@ public class UnitDataSO : ScriptableObject
     public float AttackRange => attackRange;
     public float MoveSpeed => moveSpeed;
 
-    //public AttackType //공격 타입(근거리, 원거리)
+    public AttackType AttackType => attackType;
 }
