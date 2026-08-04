0b82ae907eb8aa1d8bba7ac6ddf5799912808576
RyuCy <rcy1235@naver.com>
2026-08-04 16:10:58 +0900

    feat: Add CurrencyManager
     [Singleton.cs]
    - 글로벌 싱글톤으로 구현.
    - Find 체거 과정에서 사용 방법이 기존 강의 내용과는 다르기 때문에 스크립트의 Summary 확인 필요.
    
     [CurrencyManager.cs]
    - Singleton.cs를 상속받아 구현.
    - Add, Use 함수를 통해 증감 가능하고, Get 함수를 통해 원하는 타입의 보유량 확인 가능.
    
     [Bootstrapper.cs]
    - 개임 실행 시 실행되야할 함수들을 실행해주는 클래스 (Ex. 매니저들)
    - 현재는 씬 실행 시 강제로 실행되도록 구현되어 있지만, 추후 로딩 과정을 추가한다면 로딩 때 실행 되도록 수정할 예정.
    
    현재 추가된 재화로는 Gold가 있으며, 간단한 테스트를 통해 재화 값 증감 및 실적용 확인 완료.
    Scene 전환 시 데이터 유지 확인 완료.
    
     [ps.]
    - 각 스크립트에 Summary와 주석 작성해두었으니 활용할 때 모르겠으면 읽어보시면 될 듯 합니다.
    - Singleton 부분이 복잡해 보일 수 있으나, 시작과 동시에 하나 생성되어 있어야 하는 일반적인 매니저 스크립트라면 스크립트 작성 후 Bootstrapper.cs의 실행 함수에 추가만 해주면 됩니다.
    
     [추가 작업이 필요할 부분]
    - 저장 파트가 구현이 되면, 해당 데이터를 기반으로 초기 데이터 설정하도록 수정 필요.

---
+++ Assets/Scripts/Manager/CurrencyManager.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 재화 종류 열거
+public enum CurrencyType
+{
+    None = -1,
+    Gold, // 이후 추가될 재화 추가
+    Length
+}
+// 에디터 인스펙터 확인을 위한 구조체
+#if UNITY_EDITOR
+[Serializable]
+public struct CurrencyData
+{
+    [SerializeField] private CurrencyType type;
+    [SerializeField] private int amount;
+
+    public CurrencyType Type => type;
+    public int Amount => amount;
+
+    public CurrencyData(CurrencyType type, int amount)
+    {
+        this.type = type;
+        this.amount = amount;
+    }
+}
+#endif
+/// <summary>
+/// 재화 관리 클래스. <br/>
+/// #if - #endif 부분은 에디터 내 인스펙터에서 재화 상태를 확인하기 위한 부분으로 
+/// 이후 작업 혹은 빌드 시 UI로 표기될테니 제거 가능. <br/>
+/// 플레이어가 획득 및 사용할 재화들을 통합 관리하는 매니저 클래스. <br/>
+/// 사용법 <br/>
+/// 재화 획득: AddCurrency(재화 타입, 재화량) <br/>
+/// 재화 소모: UseCurrency(재화 타입, 재화량) <br/>
+/// 재화 확인: GetCurrency(재화 타입)
+/// </summary>
+public class CurrencyManager : Singleton<CurrencyManager>
+{
+    // 필요 없긴 한데 혹시 어떻게 적용하는지 확인해보실 분들을 위해 주석처리
+    // 씬 실행 시 강제로 매니저 생성하는 부분
+    // 로딩 구현 시 해당 파트에서 생성하는 방식으로 변경 예정
+    //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    //private static void InitializeWhenSceneStart()
+    //{
+    //    var init = Instance;
+    //}
+
+    [Header("Currency Info")]
+#if UNITY_EDITOR
+    [SerializeField] private List<CurrencyData> currencyDatas = new List<CurrencyData>();    // 재화 정보를 에디터 인스펙터에서 확인하기 위한 List
+#endif
+    private Dictionary<CurrencyType, int> currencies 
+        = new Dictionary<CurrencyType, int>();    // 재화 종류별 값 관리, 재화 종류가 적으니 최적화 보다는 사용 편리성을 위해 Dictionary 자료구조 선택
+    public event Action<CurrencyType, int> OnCurrencyChanged;     // 재화 변화 시 사용할 이벤트
+
+    // 프로퍼티
+    // 필요할까?
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        for (int i = 0; i < (int)CurrencyType.Length; i++)
+        {
+            // 재화 종류별로 초기화
+            currencies[(CurrencyType)i] = 0;
+        }
+        // 에디터 인스펙터 확인용
+#if UNITY_EDITOR
+        currencyDatas.Clear();
+        for (int i = 0; i < (int)CurrencyType.Length; i++)
+        {
+            CurrencyType type = (CurrencyType)i;
+            currencyDatas.Add(new CurrencyData(type, currencies[type]));
+        }
+#endif
+    }
+
+    // 재화 획득 함수
+    public void AddCurrency(CurrencyType type, int amount)
+    {
+        if (amount <= 0) return;
+
+        currencies[type] += amount;
+        OnCurrencyChanged?.Invoke(type, currencies[type]);
+        Debug.Log($"[재화 획득] [{type}] +{amount} | 현재 보유 : {currencies[type]}");
+
+        // 에디터 인스펙터 확인용
+#if UNITY_EDITOR
+        UpdateInspector(type, currencies[type]);
+#endif
+    }
+    // 재화 소모 함수
+    public bool UseCurrency(CurrencyType type, int amount)
+    {
+        if (currencies[type] < amount)
+        {
+            Debug.Log($"[{type}] 소지 재화가 부족합니다.");
+            return false;
+        }
+        currencies[type] -= amount;
+        OnCurrencyChanged?.Invoke(type, currencies[type]);
+        Debug.Log($"[{type}] 구매에 성공했습니다. | 남은 재화 : {currencies[type]}");
+
+        // 에디터 인스펙터 확인용
+#if UNITY_EDITOR
+        UpdateInspector(type, currencies[type]);
+#endif
+
+        return true;
+    }
+    // 현재 재화 반환 함수
+    public int GetCurrency(CurrencyType type)
+    {
+        return currencies[type];
+    }
+
+    // 에디터 인스펙터 확인용
+#if UNITY_EDITOR
+    // 에디터에서 수지 조정 시 실 적용
+    private void OnValidate()
+    {
+        if (Application.isPlaying && currencies != null)
+        {
+            for (int i = 0; i < currencyDatas.Count; i++)
+            {
+                CurrencyData data = currencyDatas[i];
+
+                if (currencies.ContainsKey(data.Type) && currencies[data.Type] != data.Amount)
+                {
+                    currencies[data.Type] = data.Amount;
+                    OnCurrencyChanged?.Invoke(data.Type, data.Amount);
+                }
+            }
+        }
+    }
+    private void UpdateInspector(CurrencyType type, int amount)
+    {
+        for (int i = 0; i < currencyDatas.Count; i++)
+        {
+            if (currencyDatas[i].Type == type)
+            {
+                currencyDatas[i] = new CurrencyData(type, amount);
+                break;
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/Reward/CurrencyTest.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// CurrencyManager 작동 확인용 클래스.
+/// </summary>
+public class CurrencyTest : MonoBehaviour
+{
+    [SerializeField] private Button addButton;
+    [SerializeField] private Button useButton;
+    [SerializeField] private TMP_Text goldText;
+
+    private void Awake()
+    {
+        CurrencyManager.Instance.OnCurrencyChanged += UpdateGoldText;
+
+        UpdateGoldText(CurrencyType.Gold, CurrencyManager.Instance.GetCurrency(CurrencyType.Gold));
+    }
+
+    public void ClickAddButton()
+    {
+        CurrencyManager.Instance.AddCurrency(CurrencyType.Gold, 100);
+    }
+    public void ClickUseButton()
+    {
+        CurrencyManager.Instance.UseCurrency(CurrencyType.Gold, 50);
+    }
+    public void ClickSceneChangeButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    private void UpdateGoldText(CurrencyType type, int amount)
+    {
+        if (type == CurrencyType.Gold)
+        {
+            goldText.text = $"Gold : {amount}";
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.OnCurrencyChanged -= UpdateGoldText;
+        }
+    }
+}
