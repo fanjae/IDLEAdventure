41bb72661a0170fb34b0e279c06561aa98e1afa7
RyuCy <rcy1235@naver.com>
2026-08-06 18:10:46 +0900

    feat: Add CSV base reward
     [CurrencyManaer.cs]
    - 채화 종류 추가
    
     [CurrencyTest.cs]
    - 추가된 재화량 표기 테스트
    
     [IdleTest.cs]
    - 확장된 재화 대응 되도록 보상 구현
    - 보상 수령 시 버려지는 시간 따로 저장해 보상 지급에 반영하도록 구현
    - CSV 파일을 읽어와 테이블 기반 보상 제공 구현
    
     [개선할 부분]
    - 현재는 재화 타입만 반영되고 있지만, 장비 상자 등 보상용 객체까지 확장할 필요성이 있음.
    - 현재 UI 표기(실시간 % 수치 변동)와 실제 보상 제공 시 남은 시간 추가 계산 파트로 인해 계산이 불필요하게 두 번 되고 있는데, 두 정보를 담을 구조체 등을 만들어 개선해볼 예정.
    
    - 그 외 수정된 스크립트는 주석 수정

--- Assets/Scripts/Manager/CurrencyManager.cs
+++ Assets/Scripts/Manager/CurrencyManager.cs
@@ -2,40 +2,22 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 재화 종류 열거
+/// <summary>
+/// 재화 종류 열거
+/// </summary>
 public enum CurrencyType
 {
     None = -1,
-    Gold, // 이후 추가될 재화 추가
+    GOLD, EXP, UPGRADE,     // 이후 추가될 재화 추가
     Length
 }
-// 에디터 인스펙터 확인을 위한 구조체
-#if UNITY_EDITOR
-[Serializable]
-public struct CurrencyData
-{
-    [SerializeField] private CurrencyType type;
-    [SerializeField] private int amount;
-
-    public CurrencyType Type => type;
-    public int Amount => amount;
-
-    public CurrencyData(CurrencyType type, int amount)
-    {
-        this.type = type;
-        this.amount = amount;
-    }
-}
-#endif
 /// <summary>
 /// 재화 관리 클래스. <br/>
-/// #if - #endif 부분은 에디터 내 인스펙터에서 재화 상태를 확인하기 위한 부분으로 
-/// 이후 작업 혹은 빌드 시 UI로 표기될테니 제거 가능. <br/>
 /// 플레이어가 획득 및 사용할 재화들을 통합 관리하는 매니저 클래스. <br/>
 /// 사용법 <br/>
 /// 재화 획득: AddCurrency(재화 타입, 재화량) <br/>
 /// 재화 소모: UseCurrency(재화 타입, 재화량) <br/>
-/// 재화 확인: GetCurrency(재화 타입)
+/// 재화 확인(값 반환): GetCurrency(재화 타입)
 /// </summary>
 public class CurrencyManager : Singleton<CurrencyManager>
 {
@@ -48,13 +30,10 @@ public class CurrencyManager : Singleton<CurrencyManager>
     //    var init = Instance;
     //}
 
-    [Header("Currency Info")]
-#if UNITY_EDITOR
-    [SerializeField] private List<CurrencyData> currencyDatas = new List<CurrencyData>();    // 재화 정보를 에디터 인스펙터에서 확인하기 위한 List
-#endif
-    private Dictionary<CurrencyType, int> currencies 
-        = new Dictionary<CurrencyType, int>();    // 재화 종류별 값 관리, 재화 종류가 적으니 최적화 보다는 사용 편리성을 위해 Dictionary 자료구조 선택
-    public event Action<CurrencyType, int> OnCurrencyChanged;     // 재화 변화 시 사용할 이벤트
+    // 재화 종류별 값 관리
+    // 재화 종류가 적으니 최적화 보다는 사용 편리성을 위해 Dictionary 자료구조 선택
+    private Dictionary<CurrencyType, int> currencies = new Dictionary<CurrencyType, int>();
+    public event Action<CurrencyType, int> OnCurrencyChanged;    // 재화 변화 시 발송할 이벤트
 
     // 프로퍼티
     // 필요할까?
@@ -63,23 +42,14 @@ public class CurrencyManager : Singleton<CurrencyManager>
     {
         base.Awake();
 
+        // 재화 초기 설정
         for (int i = 0; i < (int)CurrencyType.Length; i++)
         {
-            // 재화 종류별로 초기화
+            // 재화 종류 별로 초기화
             currencies[(CurrencyType)i] = 0;
             // 임시 세이브 매니저에 저장되어 있는 재화 받아오기.
             currencies[(CurrencyType)i] = TestSaveManager.Instance.CurrentSaveData.CurrencyDatas[i];
         }
-
-        // 에디터 인스펙터 확인용
-#if UNITY_EDITOR
-        currencyDatas.Clear();
-        for (int i = 0; i < (int)CurrencyType.Length; i++)
-        {
-            CurrencyType type = (CurrencyType)i;
-            currencyDatas.Add(new CurrencyData(type, currencies[type]));
-        }
-#endif
     }
 
     // 재화 획득 함수
@@ -95,11 +65,6 @@ public class CurrencyManager : Singleton<CurrencyManager>
         TestSaveManager.Instance.SaveGame();
 
         Debug.Log($"[재화 획득] [{type}] +{amount} | 현재 보유 : {currencies[type]}");
-
-        // 에디터 인스펙터 확인용
-#if UNITY_EDITOR
-        UpdateInspector(type, currencies[type]);
-#endif
     }
     // 재화 소모 함수
     public bool UseCurrency(CurrencyType type, int amount)
@@ -117,12 +82,7 @@ public class CurrencyManager : Singleton<CurrencyManager>
         TestSaveManager.Instance.CurrentSaveData.SetCurrency(type, currencies[type]);
         TestSaveManager.Instance.SaveGame();
 
-        Debug.Log($"[{type}] 구매에 성공했습니다. | 남은 재화 : {currencies[type]}");
-
-        // 에디터 인스펙터 확인용
-#if UNITY_EDITOR
-        UpdateInspector(type, currencies[type]);
-#endif
+        Debug.Log($"[{type}] 사용에 성공했습니다. | 남은 재화 : {currencies[type]}");
 
         return true;
     }
@@ -131,36 +91,4 @@ public class CurrencyManager : Singleton<CurrencyManager>
     {
         return currencies[type];
     }
-
-    // 에디터 인스펙터 확인용
-#if UNITY_EDITOR
-    // 에디터에서 수지 조정 시 실 적용
-    private void OnValidate()
-    {
-        if (Application.isPlaying && currencies != null)
-        {
-            for (int i = 0; i < currencyDatas.Count; i++)
-            {
-                CurrencyData data = currencyDatas[i];
-
-                if (currencies.ContainsKey(data.Type) && currencies[data.Type] != data.Amount)
-                {
-                    currencies[data.Type] = data.Amount;
-                    OnCurrencyChanged?.Invoke(data.Type, data.Amount);
-                }
-            }
-        }
-    }
-    private void UpdateInspector(CurrencyType type, int amount)
-    {
-        for (int i = 0; i < currencyDatas.Count; i++)
-        {
-            if (currencyDatas[i].Type == type)
-            {
-                currencyDatas[i] = new CurrencyData(type, amount);
-                break;
-            }
-        }
-    }
-#endif
 }
