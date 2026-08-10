3af4f4a9bfc5baf0142c9765dcd1b67c6c0de04f
502lsh <ababababa334@gmail.com>
2026-08-11 00:24:41 +0900

    feat add images intropanel,mainpanel,battlepanel

---
+++ Assets/Scripts/UI/IntroStartController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroStartController : MonoBehaviour
+{
+    public static IntroStartController Instance { get; private set; }
+
+    [SerializeField] private string mainSceneName = "MainScene";
+
+    private bool isLoading;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    public void StartGame()
+    {
+        // 중복 입력 막음
+        if (isLoading) return;
+        
+        isLoading = true;
+        SceneManager.LoadScene(mainSceneName);
+    }
+}
