ab96439baf17b650a1deae475a67dcd95bded8e1
RyuCy <63992886+Ryu-Cy@users.noreply.github.com>
2026-08-31 14:31:52 +0900

    fix: Fix 플레이어 중력 적용.
     [AdventureControllState.cs]
    - 평지 기반이라 넘어갔던 중력 적용 추가.

--- Assets/Scripts/Adventure/AdventurePlayerControlState.cs
+++ Assets/Scripts/Adventure/AdventurePlayerControlState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 /// </summary>
 public class AdventurePlayerControlState : AdventurePlayerState
 {
+    private float gravity = -9.81f;
+
     public override void OnEnter()
     {
         base.OnEnter();
@@ -29,33 +31,52 @@ public class AdventurePlayerControlState : AdventurePlayerState
 
     private void Move()
     {
+        // 중력 계산
+        // 바닥에 붙어있더라도 살짝 눌러주기
+        if (stateMachine.CharacterController.isGrounded)
+        {
+            gravity = -1.0f;
+        }
+        // 바닥에서 떨어져 있다면 중력값 적용
+        else
+        {
+            gravity += Physics.gravity.y * Time.deltaTime;
+        }
+
         Vector2 input = stateMachine.VirtualPad.InputDirection;
 
         stateMachine.SetAnimatorSpeed(input.magnitude);
 
-        if (input.sqrMagnitude < 0.01f) return;
+        Vector3 finalMoveDirection = Vector3.zero;
 
-        // 쿼터뷰 등 상황에서 이동할 때 카메라 뷰에 맞게 이동하게끔 변환을 위한 작업
-        // 카메라가 실제로 바라보는 방향 값 받아오기
-        Vector3 cameraForward = stateMachine.MainCamera.transform.forward;
-        Vector3 cameraRight = stateMachine.MainCamera.transform.right;
+        if (input.sqrMagnitude >= 0.01f)
+        {
+            // 쿼터뷰 등 상황에서 이동할 때 카메라 뷰에 맞게 이동하게끔 변환을 위한 작업
+            // 카메라가 실제로 바라보는 방향 값 받아오기
+            Vector3 cameraForward = stateMachine.MainCamera.transform.forward;
+            Vector3 cameraRight = stateMachine.MainCamera.transform.right;
 
-        // Y축 고정
-        // 바닥에 붙어서 다녀야하기 때문
-        cameraForward.y = 0.0f;
-        cameraRight.y = 0.0f;
+            // 카메라 방향 벡터 Y축 고정
+            // 바닥에 붙어서 다녀야하기 때문
+            cameraForward.y = 0.0f;
+            cameraRight.y = 0.0f;
 
-        // 방향 벡터를 정규화
-        cameraForward.Normalize();
-        cameraRight.Normalize();
+            // 방향 벡터를 정규화
+            cameraForward.Normalize();
+            cameraRight.Normalize();
 
-        // 카메라 방향을 기준으로 패드 입력값을 받아 최종 이동 방향 벡터 계산
-        Vector3 moveDirection = (cameraForward * input.y) + (cameraRight * input.x);
+            // 카메라 방향을 기준으로 패드 입력값을 받아 최종 이동 방향 벡터 계산
+            Vector3 moveDirection = (cameraForward * input.y) + (cameraRight * input.x);
 
-        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, stateMachine.RotationSpeed * Time.deltaTime);
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, stateMachine.RotationSpeed * Time.deltaTime);
+
+            finalMoveDirection = moveDirection * stateMachine.MoveSpeed;
+        }
+        // 중력 적용
+        finalMoveDirection.y = gravity;
 
         // 캐릭터 컨트롤러의 Move 함수를 통한 이동
-        stateMachine.CharacterController.Move(moveDirection * stateMachine.MoveSpeed * Time.deltaTime);
+        stateMachine.CharacterController.Move(finalMoveDirection * Time.deltaTime);
     }
 }
